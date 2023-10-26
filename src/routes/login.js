00e6bf7806a965d94e661a68a9ef28c470b99997
Carlos <hencarlos42@gmail.com>
2023-10-26 11:18:49 -0300

    Adicionando monitpramento

--- src/routes/login.js
+++ src/routes/login.js
@@ -18,6 +18,7 @@ const routes = express.Router();
  *       400:
  *         description: Erro ao obter informações
  */
+
 routes.get('/login', controller.get);
 
 /**
@@ -53,8 +54,26 @@ routes.get('/login', controller.get);
  *       400:
  *         description: Requisição mal formatada
  */
+
 routes.post('/', controller.post);
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     tags:
+ *       - Login
+ *     description: Obtém informações da página de login ou qualquer outra ação relacionada.
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Sucesso ao obter informações da página de login
+ *       400:
+ *         description: Erro ao obter informações
+ */
 
 routes.post('/login',controller.get)
+
+
 module.exports = routes;
