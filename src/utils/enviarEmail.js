e344bcef764da2b2762c92342e7bfd0e2a6d9b10
Carlos Henrique Venâncio da Costa <132011061+carlos-venancio@users.noreply.github.com>
2023-12-11 13:23:10 -0300

    Configurando gmail para enviar email

--- src/utils/enviarEmail.js
+++ src/utils/enviarEmail.js
@@ -1,52 +1,67 @@
-'use strict'
-require('dotenv').config()
+"use strict";
+require("dotenv").config();
 
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 
-var transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_FROM,
-      clientId: process.env.OAUTH_CLIENTEID,
-      clientSecret: process.env.OAUTH_CLIENTE_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+const redirect_uri = "https://developers.google.com/oauthplayground";
+
+// objeto que armazena todos os dados de credenciais
+const googleCredentials = {
+  user: process.env.EMAIL_FROM,
+  clientId: process.env.OAUTH_CLIENTEID,
+  clientSecret: process.env.OAUTH_CLIENTE_SECRET,
+  refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+};
+
+// Consulta um cliente
+const oAuth2Client = new google.auth.OAuth2(
+  googleCredentials.clientId,
+  googleCredentials.clientSecret,
+  redirect_uri
+);
+
+async function enviarEmailRedefinirSenha(emailDestinatario, codigo) {
+  try {
+    const acessToken = await oAuth2Client
+      .setCredentials({ refresh_token: googleCredentials.refreshToken })
+      .getAccessToken();
+
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "oAuth2",
+        ...googleCredentials,
+        acessToken,
+      },
+    });
+
+    const email = await transport.sendMail({
+      from: process.env.EMAIL_FROM,
+      to: emailDestinatario,
+      subject: "Redefina sua senha",
+      text: `Codigo para recuperação da senha: ${codigo}`,
+    });
+
+    // caso o email não for enviado
+    if (!email.accepted.length) {
+      throw new Error("Falha ao enviar o email");
     }
-});
-
-async function enviarEmailRedefinirSenha(emailDestinatario, codigo){
-
-    try{
-
-        const email = await transport.sendMail({
-            from: process.env.EMAIL_FROM,
-            to: emailDestinatario,
-            subject: 'Redefina sua senha',
-            text: `Codigo para recuperação da senha: ${codigo}` 
-        })
-    
-        // caso o email não for enviado
-        if (!email.accepted.length) {
-            throw new Error('Falha ao enviar o email')
-       }
-    }
-
-    catch(e) {
-        console.log(e)
-    }
-
+  } catch (e) {
+    console.log(e);
+  }
 }
-
 
 // transport.sendMail({
 //     from: process.env.EMAIL_FROM,
 //     to: "ipp.carloshenrique@gmail.com",
 //     subject: 'Redefina sua senha',
-//     text: `Codigo para recuperação da senha: 123456` 
+//     text: `Codigo para recuperação da senha: 123456`
 // })
 
 // .then((data) => console.log(data))
 // .catch(error => console.log(error))
 
 module.exports = {
-    enviarEmailRedefinirSenha
-}
+  enviarEmailRedefinirSenha,
+};
